a7894c8849ec56b5ed6aad0ed9d721e8b89e5f79
nacho <nacho@Nacho-Lubuntu>
2018-10-28 18:30:25 -0300

    agregue archivos de la funcion

--- TP11/main.c
+++ TP11/main.c
@@ -1,24 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * TP11 EJERCICIO 2
  */
 
 /* 
  * File:   main.c
  * Author: nacho
- *
- * Created on 28 de octubre de 2018, 17:41
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "palindromo.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+int main(int argc, char** argv) 
+{
+    char caracter;
+    while((caracter=getchar() != '\n'))
+    {
+        
+    }
+    return 0;
 }
 
