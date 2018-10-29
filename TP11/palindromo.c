fa8f572664eacda3f920d67a93fc08f20722a88e
fran <fran@fran-VirtualBox>
2018-10-29 00:49:15 -0300

    ahi va wacho

--- TP11/palindromo.c
+++ TP11/palindromo.c
@@ -8,32 +8,18 @@
 
 #include "palindromo.h"
 
-int palindromo (const char *str)
-{s
-    int i=0,j=0,cambio=0;
-    char mat1 [100];
-    char mat2 [100];
-    if (*str == '\0')
-    {
-        cambio = 1;
-        return 0;
-    }
-    else
-    {
-        int len;
-        
-        if (cambio == 0)
-        {
-            mat1 [j] = len;
-            j++;
-        }
-        
-        len = palindromo(str+1);
-        mat2 [i] = len;
-            i++;
-      
-        return len;
-    }
+int palindromo (const char *str,int len)
+{
+	if ((*str == *(str[len-1])) && (*str != '\0'))
+	{	
+		int res;
+		res = palindromo (str+1, len-1);
+		printf ("valido");
+		return res;
+	}
+	else
+	{
+		printf ("invalido");
+		return 0;
+	}
 }
-
-
