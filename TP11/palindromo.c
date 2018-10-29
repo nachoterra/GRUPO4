37a49b8c2a5a42630b70fadad49ca29699bd9d54
fran <fran@fran-VirtualBox>
2018-10-28 23:08:04 -0300

    ahi va wacho

--- TP11/palindromo.c
+++ TP11/palindromo.c
@@ -10,29 +10,27 @@
 
 int palindromo (const char *str)
 {s
-    int resultado,i=0,j=0,cambio=0;
+    int i=0,j=0,cambio=0;
     char mat1 [100];
     char mat2 [100];
     if (*str == '\0')
     {
-        cambio = 2;
+        cambio = 1;
         return 0;
     }
     else
     {
         int len;
-        len = 1 + palindromo(str+1);
-        if (cambio = 0)
+        
+        if (cambio == 0)
         {
             mat1 [j] = len;
             j++;
         }
-        else
-        {
-            mat2 [i] = len;
+        
+        len = palindromo(str+1);
+        mat2 [i] = len;
             i++;
-        }
-        len = 1 + palindromo(str+1);
       
         return len;
     }
