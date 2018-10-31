763fa3cd3ff6d0aac3bf5f21461c8979b369a5f7
nacho <nacho@Nacho-Lubuntu>
2018-10-31 09:56:03 -0300

    tp nuevo

---
+++ TP12/TP DRIVERS/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: nacho
+ *
+ * Created on 31 de octubre de 2018, 09:55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
